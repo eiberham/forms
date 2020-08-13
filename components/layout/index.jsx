3cd9d4feb9d577fe05a2cfc1f5a98bc93795879a
Abraham <cedenoabraham@gmail.com>
2020-08-13 04:21:14 -0300

    fix eslint rules
    more styling

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Layout = ({ children }) => {
-    return <div className="container">{ children }</div>
+    return <div className="container">{children}</div>
 }
-
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
 export default Layout
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Button } from 'antd'
 import Layout from '../components/layout'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
 export default function Home() {
-  return (
-    <React.StrictMode>
-      <Layout>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="content-language" content="en" />
-          <title>Forms</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" src="https://fonts.googleapis.com/css?family=Roboto" />
-        </Head>
-        <Header />
-        <section className="slogan">
-          <h1>Online Digital Forms</h1>
-          <div className="slogan-text">
-            Upload and configure your digital form fields online, see result in live view, fill forms out and get the outcome.
-          </div>
-          <Button type="primary" size="large">Start</Button>
-        </section>
-        <Footer />
-      </Layout>
-    </React.StrictMode>
-  )
+    return (
+        <React.StrictMode>
+            <Layout>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="content-language" content="en" />
+                    <title>Forms</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="stylesheet"
+                        src="https://fonts.googleapis.com/css?family=Roboto"
+                    />
+                </Head>
+                <Header />
+                <section className="slogan">
+                    <h1>Online Digital Forms</h1>
+                    <div className="slogan-text">
+                        Upload and configure your digital form fields online,
+                        see result in live view, fill forms out and get the
+                        outcome.
+                    </div>
+                    <button type="button" className="btn --primary">
+                        Get Started
+                    </button>
+                </section>
+                <Footer />
+            </Layout>
+        </React.StrictMode>
+    )
 }
